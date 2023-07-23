2e25d722d81ae02e3b208abba12c754a1c4bedfd
K-ima <bulgin.a.v@bk.ru>
2023-07-23 12:59:45 +0300

    Finish logic

--- Lesson 2.02/ViewController.swift
+++ Lesson 2.02/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
 
     @IBOutlet var redSignal: UIView!
     
-    @IBOutlet var yellowSiglan: UIView!
+    @IBOutlet var yellowSignal: UIView!
     
     @IBOutlet var greenSignal: UIView!
     
@@ -21,10 +21,30 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         showButton.layer.cornerRadius = 10
+        
+        if redSignal.alpha == 0.3 && yellowSignal.alpha == 0.3 && greenSignal.alpha == 0.3 {
+            showButton.setTitle("START", for: .normal)
+        } else {
+            showButton.setTitle("NEXT", for: .normal)
+        }
     }
 
-    @IBAction func changeNameButton(_ sender: UIButton) {
- 
+    @IBAction func actionButton(_ sender: UIButton) {
+        switch (redSignal.alpha, yellowSignal.alpha, greenSignal.alpha) {
+        case (0.3, 0.3, 0.3):
+            redSignal.alpha = 1
+        case (1, 0.3, 0.3):
+            redSignal.alpha = 0.3
+            yellowSignal.alpha = 1
+        case (0.3, 1, 0.3):
+            yellowSignal.alpha = 0.3
+            greenSignal.alpha = 1
+        case (0.3, 0.3, 1):
+            greenSignal.alpha = 0.3
+            redSignal.alpha = 1
+        default: print("Ошибка")
+        }
     }
 }
+
 

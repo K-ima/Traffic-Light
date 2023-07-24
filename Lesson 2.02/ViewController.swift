caf42998bbf5101c84c96f6ccb0683f3d339a49c
K-ima <bulgin.a.v@bk.ru>
2023-07-24 21:07:03 +0300

    Begin fix after lecture

--- Lesson 2.02/ViewController.swift
+++ Lesson 2.02/ViewController.swift
@@ -7,29 +7,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
     
-    @IBOutlet weak var showButton: UIButton!
-    
+    @IBOutlet var showButton: UIButton!
     @IBOutlet var redSignal: UIView!
-    
     @IBOutlet var yellowSignal: UIView!
-    
     @IBOutlet var greenSignal: UIView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         showButton.layer.cornerRadius = 10
-        redSignal.layer.cornerRadius = 45
-        yellowSignal.layer.cornerRadius = 45
-        greenSignal.layer.cornerRadius = 45
+        redSignal.layer.cornerRadius = redSignal.frame.width / 2
+        yellowSignal.layer.cornerRadius = yellowSignal.frame.width / 2
+        greenSignal.layer.cornerRadius = greenSignal.frame.width / 2
     }
     
     @IBAction func actionButton(_ sender: UIButton) {
+        sender.setTitle("NEXT", for: .normal)
         
         if redSignal.alpha != 1 && yellowSignal.alpha != 1 && greenSignal.alpha != 1{
-            sender.setTitle("NEXT", for: .normal)
             redSignal.alpha = 1
         } else if redSignal.alpha == 1 {
             redSignal.alpha = 0.3
